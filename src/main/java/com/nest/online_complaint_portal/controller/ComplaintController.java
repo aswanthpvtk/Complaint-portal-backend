94eb376dc0abca21d5da190a27fd74741a30fbeb
Aswanth P <Aswanth@NTP-LAP-734.chn.nestgroup.net>
2022-12-09 14:32:23 +0530

    com

--- src/main/java/com/nest/online_complaint_portal/controller/ComplaintController.java
+++ src/main/java/com/nest/online_complaint_portal/controller/ComplaintController.java
@@ -1,18 +1,25 @@
 package com.nest.online_complaint_portal.controller;
 
+import com.nest.online_complaint_portal.dao.ComplaintDao;
+import com.nest.online_complaint_portal.model.Complaint;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
+import java.util.List;
 
 @RestController
 public class ComplaintController {
 
+    @Autowired
+    private ComplaintDao dao;
+
     @CrossOrigin(origins = "*")
     @PostMapping(path = "/ureg",consumes = "application/json",produces = "application/json")
-    public HashMap<String, String> Ureg(@RequestBody User u){
+    public HashMap<String, String> Ureg(@RequestBody Complaint u){
 
         dao.save(u);
         HashMap<String, String> map = new HashMap<>();
@@ -20,4 +27,28 @@ public class ComplaintController {
 
         return map;
     }
+
+    @CrossOrigin(origins = "*")
+    @PostMapping(path = "/userLogin", produces = "application/json", consumes = "application/json")
+    public HashMap<String, String> UserLogin(@RequestBody Complaint u)
+    {
+        System.out.println(u.getEmail());
+        System.out.println(u.getPsw());
+        List<Complaint> result = (List<Complaint>) dao.login(u.getEmail(), u.getPsw());
+        HashMap<String, String> st = new HashMap<>();
+        if (result.size() == 0)
+        {
+            st.put("status", "failed");
+            st.put("message", "user doesn't exist");
+        } else
+        {
+            int id = result.get(0).getId();
+            st.put("userId", String.valueOf(id));
+            st.put("status", "success");
+            st.put("message", "d");
+        }
+
+        return st;
+    }
+
 }
--- src/main/java/com/nest/online_complaint_portal/model/Complaint.java
+++ src/main/java/com/nest/online_complaint_portal/model/Complaint.java
@@ -6,13 +6,96 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "complaints")
+@Table(name = "user")
 public class Complaint {
 
     @Id
     @GeneratedValue
-    private
+    private int id;
+    private String name;
+    private String phone;
+    private String address;
+    private String email;
+    private String uname;
+    private String psw;
+    private String cpsw;
 
 
+    public Complaint() {
+    }
 
+    public Complaint(int id, String name, String phone, String address, String email, String uname, String psw, String cpsw) {
+        this.id = id;
+        this.name = name;
+        this.phone = phone;
+        this.address = address;
+        this.email = email;
+        this.uname = uname;
+        this.psw = psw;
+        this.cpsw = cpsw;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUname() {
+        return uname;
+    }
+
+    public void setUname(String uname) {
+        this.uname = uname;
+    }
+
+    public String getPsw() {
+        return psw;
+    }
+
+    public void setPsw(String psw) {
+        this.psw = psw;
+    }
+
+    public String getCpsw() {
+        return cpsw;
+    }
+
+    public void setCpsw(String cpsw) {
+        this.cpsw = cpsw;
+    }
 }
